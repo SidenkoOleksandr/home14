c9e51f6dd44465ae622f41f34ae486df4cfb8b64
Sidenko_Oleksandr <sidoj@meta.ua>
2019-01-20 09:01:23 +0200

    home14

---
+++ data.php
@@ -0,0 +1,166 @@
+<?php
+return [
+    'mainMenu' => [
+        ['title' => 'Home',
+            'url' => '#'],
+        ['title' => 'About us',
+            'url' => '#'],
+        ['title' => 'Offer',
+            'url' => '#'],
+        ['title' => 'Portfolio',
+            'url' => '#'],
+        ['title' => 'Contact',
+            'url' => '#'],
+    ],
+    'mainLog' => [
+        'imgLogo' => 'assets/img/logo.png',
+        'url' => '#',
+        'alt' => 'InteractiveAgency logo',
+    ],
+    'headerContent' => [
+        'heading' => 'Lorem ipsum dolor sit amet',
+        'paragr' => 'Nunc vel nibh tempor, pharetra lectus congue, luctus orci.',
+        'buttonBuy' => [
+            'title' => 'Ask for price',
+            'url' => '#',
+        ],
+        'buttonMouse' => [
+            'icon' => 'assets/img/icon_mouse.png',
+            'url' => '#',
+        ],
+        'buttonScrool' => [
+            'title' => 'scrool down',
+            'url' => '#',
+        ],
+    ],
+    'BestDesign' => [
+        'heading' => 'BestDesign',
+        'paragr' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                Curabitur ullamcorper auctor mauris, placerat fermentum lectus vulputate sed.
+                Phasellus vestibulum sit amet dolor eget eleifend.',
+        'buttonBuy' => [
+            'title' => 'Ask for price',
+            'url' => '#',
+        ],
+    ],
+    'BestCode' => [
+        'heading' => 'BestCode',
+        'paragr' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                Curabitur ullamcorper auctor mauris, placerat fermentum lectus vulputate sed.
+                Phasellus vestibulum sit amet dolor eget eleifend.',
+        'buttonBuy' => [
+            'title' => 'Ask for price',
+            'url' => '#',
+        ],
+    ],
+
+    'what_we_do' => [
+        'heading' => 'What we do?',
+        ['title' => 'Web design',
+            'icon' => ['iconImg' => 'assets/img/icon_WebDesign.png',
+                'alt' => 'icon_WebDesign',
+                'url' => '#',
+            ],
+            'paragr' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                Curabitur ullamcorper auctor mauris, placerat fermentum lectus vulputate sed.
+                Phasellus vestibulum sit amet dolor eget eleifend.',
+        ],
+        ['title' => 'Digital paiting',
+            'icon' => ['iconImg' => 'assets/img/icon_DigitalPaiting.png',
+                'alt' => 'icon_DigitalPaiting',
+                'url' => '#'],
+            'paragr' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                Curabitur ullamcorper auctor mauris, placerat fermentum lectus vulputate sed.
+                Phasellus vestibulum sit amet dolor eget eleifend.',
+        ],
+        ['title' => 'Web Applications',
+            'icon' => ['iconImg' => 'assets/img/icon_WebApplications.png',
+                'alt' => 'icon_WebApplications',
+                'url' => '#',
+            ],
+            'paragr' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                Curabitur ullamcorper auctor mauris, placerat fermentum lectus vulputate sed.
+                Phasellus vestibulum sit amet dolor eget eleifend.',
+        ],
+        ['title' => 'Desktop Applications',
+            'icon' => ['iconImg' => 'assets/img/icon_DesktopApplications.png',
+                'alt' => 'icon_DesktopApplications',
+                'url' => '#',
+            ],
+            'paragr' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                Curabitur ullamcorper auctor mauris, placerat fermentum lectus vulputate sed.
+                Phasellus vestibulum sit amet dolor eget eleifend.',
+        ],
+        'buttonBuy' => [
+            'title' => 'Ask for price',
+            'url' => '#',
+        ],
+    ],
+    'app' => [
+        ['heading' => 'Neptune template',
+            'paragr' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                 Curabitur ullamcorper auctor mauris, placerat fermentum.',
+            'img' => './assets/img/droneZone.png',
+            'imgAlt' => 'droneZone',
+            'url' => '#',
+        ],
+        ['heading' => 'Neptune template',
+            'paragr' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                Curabitur ullamcorper auctor mauris, placerat fermentum.',
+            'img' => './assets/img/profile.png',
+            'imgAlt' => 'profile',
+            'url' => '#',
+        ],
+        ['heading' => 'Neptune template',
+            'paragr' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                Curabitur ullamcorper auctor mauris, placerat fermentum.',
+            'img' => './assets/img/fitApp.png',
+            'imgAlt' => 'fitApp',
+            'url' => '#',
+        ],
+    ],
+    'about_us' => [
+        'heading' => 'About us!',
+        'paragr' => 'Aliquam sagittis neque in lectus semper, nec elementum arcu scelerisque.
+                Curabitur ullamcorper auctor mauris, placerat fermentum.',
+        ['name' => 'John Doe',
+            'photo' => './assets/img/first_member.png',
+            'profession' => 'Curabitur ullamcorper auctor mauris, placerat fermentum.',
+            'url' => '#',
+            'photoAlt' => 'first_member',
+        ],
+        ['name' => 'John Doe',
+            'photo' => './assets/img/second_member.png',
+            'profession' => 'Curabitur ullamcorper auctor mauris, placerat fermentum.',
+            'url' => '#',
+            'photoAlt' => 'second_member',
+        ],
+        ['name' => 'John Doe',
+            'photo' => './assets/img/third_member.png',
+            'profession' => 'Curabitur ullamcorper auctor mauris, placerat fermentum.',
+            'url' => '#',
+            'photoAlt' => 'third_member',
+        ],
+        ['name' => 'John Doe',
+            'photo' => './assets/img/fourth_member.png',
+            'profession' => 'Curabitur ullamcorper auctor mauris, placerat fermentum.',
+            'url' => '#',
+            'photoAlt' => 'fourth_member',
+        ],
+        'buttonWatch' => [
+            'title' => 'Watch more',
+            'url' => '#',
+        ],
+    ],
+
+    'buttonWatch' => [
+        'title' => 'Watch more',
+        'url' => '#',
+    ],
+    'map' => 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387190.27989910427!2d-74.25987062089824!3d40.69767006630391!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2z0J3RjNGOLdCZ0L7RgNC6LCDQodCo0JA!5e0!3m2!1sru!2sua!4v1545637120108',
+
+    'footerParagr' => 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        Try awsome tool for desgners symu.co',
+
+]
+?>
---
+++ index.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php $data = require('data.php');?>
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-expand-sm navbar-light container">
+        <a class="navbar-brand" href="<?php echo $data['mainLog']['url'] ?>">
+            <img src="<?php echo $data['mainLog']['imgLogo'] ?>" alt="<?php echo $data['mainLog']['alt'] ?>">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto mainMenu">
+                <?php $i = 0; ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><?php echo $data['mainMenu'][$i]['title'] ?>
+                        <span class="sr-only"></span>
+                    </a>
+                    <?php $i++ ?>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><?php echo $data['mainMenu'][$i]['title'] ?></a>
+                    <?php $i++ ?>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><?php echo $data['mainMenu'][$i]['title'] ?></a>
+                    <?php $i++ ?>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><?php echo $data['mainMenu'][$i]['title'] ?></a>
+                    <?php $i++ ?>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><?php echo $data['mainMenu'][$i]['title'] ?></a>
+                    <?php $i++ ?>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <section class="header-content d-flex flex-column col-12">
+        <h1><?php echo $data['headerContent']['heading'] ?></h1>
+        <p><?php echo $data['headerContent']['paragr'] ?></p>
+        <a class="btn mx-auto px-4 m-4" href="<?php echo $data['headerContent']['buttonBuy']['url'] ?>"
+           role="button"><?php echo $data['headerContent']['buttonBuy']['title'] ?>
+        </a>
+        <a class="mx-auto px-4" href="<?php echo $data['headerContent']['buttonMouse']['url'] ?>">
+            <img src="<?php echo $data['headerContent']['buttonMouse']['icon'] ?>" alt="icon_mouse">
+        </a>
+        <a class="mx-auto px-4 m-2" href="<?php echo $data['headerContent']['buttonScrool']['url'] ?>">
+            <?php echo $data['headerContent']['buttonScrool']['title'] ?>
+        </a>
+    </section>
+</header>
+<main>
+
+    <section class="best-design col-sm-12 col-md-6">
+        <div class="wrap-best-design container">
+            <h2><?php echo $data['BestDesign']['heading'] ?></h2>
+            <p>
+                <?php echo $data['BestDesign']['paragr'] ?>
+            </p>
+            <a class="btn mx-auto px-4 m-4" href="<?php echo $data['BestDesign']['buttonBuy']['url'] ?>" role="button">
+                <?php echo $data['BestDesign']['buttonBuy']['title'] ?>
+            </a>
+
+        </div>
+    </section>
+
+    <section class="best-code col-sm-12 col-md-6">
+        <div class="wrap-best-code container">
+            <h2><?php echo $data['BestCode']['heading'] ?></h2>
+            <p>
+                <?php echo $data['BestCode']['paragr'] ?>
+            </p>
+            <a class="btn mx-auto px-4 m-4" href="<?php echo $data['BestCode']['buttonBuy']['url'] ?>" role="button">
+                <?php echo $data['BestCode']['buttonBuy']['title'] ?>
+            </a>
+        </div>
+    </section>
+    <section class=" what-we-do container">
+        <?php $i = 0; ?>
+        <h2><?php echo $data['what_we_do']['heading'] ?></h2>
+        <ul class="row">
+            <li class="col-md-6">
+                <a href="<?php echo $data['what_we_do'][$i]['icon']['url'] ?>">
+                    <img src="<?php echo $data['what_we_do'][$i]['icon']['iconImg'] ?>"
+                         alt="<?php echo $data['what_we_do'][$i]['icon']['alt'] ?>">
+                </a>
+                <h3><?php echo $data['what_we_do'][$i]['title'] ?></h3>
+                <p>
+                    <?php echo $data['what_we_do'][$i]['paragr'] ?>
+                </p>
+                <?php $i++ ?>
+            </li>
+            <li class="col-md-6">
+                <a href="<?php echo $data['what_we_do'][$i]['icon']['url'] ?>">
+                    <img src="<?php echo $data['what_we_do'][$i]['icon']['iconImg'] ?>"
+                         alt="<?php echo $data['what_we_do'][$i]['icon']['alt'] ?>">
+                </a>
+                <h3><?php echo $data['what_we_do'][$i]['title'] ?></h3>
+                <p>
+                    <?php echo $data['what_we_do'][$i]['paragr'] ?>
+                </p>
+                <?php $i++ ?>
+            </li>
+            <li class="col-md-6">
+                <a href="<?php echo $data['what_we_do'][$i]['icon']['url'] ?>">
+                    <img src="<?php echo $data['what_we_do'][$i]['icon']['iconImg'] ?>"
+                         alt="<?php echo $data['what_we_do'][$i]['icon']['alt'] ?>">
+                </a>
+                <h3><?php echo $data['what_we_do'][$i]['title'] ?></h3>
+                <p>
+                    <?php echo $data['what_we_do'][$i]['paragr'] ?>
+                </p>
+                <?php $i++ ?>
+            </li>
+            <li class="col-md-6">
+                <a href="<?php echo $data['what_we_do'][$i]['icon']['url'] ?>">
+                    <img src="<?php echo $data['what_we_do'][$i]['icon']['iconImg'] ?>"
+                         alt="<?php echo $data['what_we_do'][$i]['icon']['alt'] ?>">
+                </a>
+                <h3><?php echo $data['what_we_do'][$i]['title'] ?></h3>
+                <p>
+                    <?php echo $data['what_we_do'][$i]['paragr'] ?>
+                </p>
+                <?php $i++ ?>
+            </li>
+        </ul>
+        <a class="btn mx-auto px-4 m-4" href="<?php echo $data['what_we_do']['buttonBuy']['title'] ?>"
+           role="button"><?php echo $data['what_we_do']['buttonBuy']['title'] ?>
+        </a>
+    </section>
+
+    <section class="app">
+        <?php $i = 0; ?>
+        <a href="<?php echo $data['app'][$i]['url'] ?>">
+            <img src="<?php echo $data['app'][$i]['img'] ?>" alt="<?php echo $data['app'][$i]['imgAlt'] ?>">
+            <div class="hide" style="position: absolute;">
+                <h2><?php echo $data['app'][$i]['heading'] ?></h2>
+                <p>
+                    <?php echo $data['app'][$i]['paragr'] ?>
+                </p>
+            </div>
+            <?php $i++ ?>
+        </a>
+        <a href="<?php echo $data['app'][$i]['url'] ?>">
+            <img src="<?php echo $data['app'][$i]['img'] ?>" alt="<?php echo $data['app'][$i]['imgAlt'] ?>">
+            <div class="hide" style="position: absolute;">
+                <h2><?php echo $data['app'][$i]['heading'] ?></h2>
+                <p>
+                    <?php echo $data['app'][$i]['paragr'] ?>
+                </p>
+            </div>
+            <?php $i++ ?>
+        </a>
+        <a href="<?php echo $data['app'][$i]['url'] ?>">
+            <img src="<?php echo $data['app'][$i]['img'] ?>" alt="<?php echo $data['app'][$i]['imgAlt'] ?>">
+            <div class="hide" style="position: absolute;">
+                <h2><?php echo $data['app'][$i]['heading'] ?></h2>
+                <p>
+                    <?php echo $data['app'][$i]['paragr'] ?>
+                </p>
+            </div>
+            <?php $i++ ?>
+        </a>
+    </section>
+    <section class="about_us">
+        <?php $i = 0; ?>
+        <h2><?php echo $data['about_us']['heading'] ?></h2>
+        <p>
+            <?php echo $data['about_us']['paragr'] ?>
+        </p>
+        <ul>
+            <li class="item-members">
+                <a href="<?php echo $data['about_us'][$i]['url'] ?>">
+                    <img src="<?php echo $data['about_us'][$i]['photo'] ?>"
+                         alt="<?php echo $data['about_us'][$i]['photoAlt'] ?>">
+                    <h3><?php echo $data['about_us'][$i]['name'] ?></h3>
+                    <p><?php echo $data['about_us'][$i]['profession'] ?></p>
+                </a>
+                <?php $i++ ?>
+            </li>
+            <li class="item-members">
+                <a href="<?php echo $data['about_us'][$i]['url'] ?>">
+                    <img src="<?php echo $data['about_us'][$i]['photo'] ?>"
+                         alt="<?php echo $data['about_us'][$i]['photoAlt'] ?>">
+                    <h3><?php echo $data['about_us'][$i]['name'] ?></h3>
+                    <p><?php echo $data['about_us'][$i]['profession'] ?></p>
+                </a>
+                <?php $i++ ?>
+            </li>
+            <li class="item-members">
+                <a href="<?php echo $data['about_us'][$i]['url'] ?>">
+                    <img src="<?php echo $data['about_us'][$i]['photo'] ?>"
+                         alt="<?php echo $data['about_us'][$i]['photoAlt'] ?>">
+                    <h3><?php echo $data['about_us'][$i]['name'] ?></h3>
+                    <p><?php echo $data['about_us'][$i]['profession'] ?></p>
+                </a>
+                <?php $i++ ?>
+            </li>
+            <li class="item-members">
+                <a href="<?php echo $data['about_us'][$i]['url'] ?>">
+                    <img src="<?php echo $data['about_us'][$i]['photo'] ?>"
+                         alt="<?php echo $data['about_us'][$i]['photoAlt'] ?>">
+                    <h3><?php echo $data['about_us'][$i]['name'] ?></h3>
+                    <p><?php echo $data['about_us'][$i]['profession'] ?></p>
+                </a>
+                <?php $i++ ?>
+            </li>
+        </ul>
+        <a class="btn mx-auto px-4 m-4" href="<?php echo $data['about_us']['buttonWatch']['url'] ?>" role="button">
+            <?php echo $data['about_us']['buttonWatch']['title'] ?>
+        </a>
+    </section>
+    <section class="map">
+        <iframe src="<?php echo $data['map'] ?>"
+                width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </section>
+</main>
+
+<footer>
+    <p>
+        <?php echo $data['footerParagr'] ?>
+    </p>
+</footer>
+
+<script src="assets/js/libs.js"></script>
+<script src="assets/js/main.js"></script>
+</body>
+</html>
